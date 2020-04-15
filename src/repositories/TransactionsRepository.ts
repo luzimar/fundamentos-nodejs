d4fdebde0b9c35d69f3dde678e4ec9fc58ec2a78
luzimar <lzm.spfc@hotmail.com>
2020-04-15 19:59:41 -0300

    Conclusão do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
 import Transaction from '../models/Transaction';
+import GetValueFromType from '../utils/GetValueFromType';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDto {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = GetValueFromType({
+      transactions: this.transactions,
+      type: 'income',
+    });
+
+    const totalOutcome = GetValueFromType({
+      transactions: this.transactions,
+      type: 'outcome',
+    });
+
+    const total = totalIncome - totalOutcome;
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDto): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
